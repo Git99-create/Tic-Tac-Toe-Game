1af12b9ac349ac3eab02d6ff4270e715525ad069
Git99-create <iqra.252265.co@mhssce.ac.in>
2026-01-11 18:48:03 +0530

    tic tac toe game in python

---
+++ t.py
@@ -0,0 +1,43 @@
+board = [' ' for _ in range(9)]
+
+def print_board():
+    print()
+    print(board[0], '|', board[1], '|', board[2])
+    print('--+---+--')
+    print(board[3], '|', board[4], '|', board[5])
+    print('--+---+--')
+    print(board[6], '|', board[7], '|', board[8])
+    print()
+
+def check_win(player):
+    win_positions = [
+        (0,1,2),(3,4,5),(6,7,8),
+        (0,3,6),(1,4,7),(2,5,8),
+        (0,4,8),(2,4,6)
+    ]
+    for pos in win_positions:
+        if board[pos[0]] == board[pos[1]] == board[pos[2]] == player:
+            return True
+    return False
+
+player = 'X'
+
+for turn in range(9):
+    print_board()
+    move = int(input(f"Player {player}, enter position (1-9): ")) - 1
+
+    if board[move] != ' ':
+        print("Position already taken! Try again.")
+        continue
+
+    board[move] = player
+
+    if check_win(player):
+        print_board()
+        print(f"🎉 Player {player} wins!")
+        break
+
+    player = 'O' if player == 'X' else 'X'
+else:
+    print_board()
+    print("😐 It's a draw!")
